9d1ffd88742a3f7076c0ffcdd4a2f70a3dfeaba6
Andynopol <41628030+Andynopol@users.noreply.github.com>
2020-09-10 03:00:19 +0300

    Img are saved to a file

--- BE/index.js
+++ BE/index.js
@@ -1,5 +1,12 @@
 const express = require( 'express' );
 const Datastore = require( 'nedb' );
+const Name = require('./name');
+const fs = require('fs');
+const uploads = './uploads';
+const imgSizeOf = require('buffer-image-size');
+
+
+
 
 const app = express();
 const database = new Datastore( 'data.db' );
@@ -14,20 +21,49 @@ app.use( express.json( {
 
 app.post( '/api', ( req, res ) => {
 	const data = req.body;
+	const base64Image = data.img;
+	const matches = base64Image.match( /^data:([A-Za-z-+\/]+);base64,(.+)$/ );
+	if ( matches.length !== 3 ) {
+		console.log( 'INVALID' )
+	}
+	const img = {};
+	img.type = matches[ 1 ];
+	img.data = new Buffer.from(matches[ 2 ], 'base64');
+
+	const extension = img.type.substr(img.type.length-3, img.type.length-1);
+	img.name = `${Name.makeid()}.${extension}`;
+	
+	const path = `${uploads}/${img.name}`;
+	
+
+    fs.writeFile(path, img.data, function(err){
+		if(err)
+		{
+			console.log(err.message);
+		}
+	});
+	
+
+	data.img = {path: path,name: img.name};
+
+
+
 	const timestamp = Date.now();
 	data.timestamp = timestamp;
 	database.insert( data );
-	console.log( req.body );
+	// console.log( req.body );
 	res.json( data );
 } );
 
-app.get('/api', (req, res)=>{
-	database.find({}, (err, data)=>{
-		if(err){
-			res.json({'status':'Internal Error'});
+app.get( '/api', ( req, res ) => {
+	database.find( {}, ( err, data ) => {
+		if ( err ) {
+			res.json( {
+				'status': 'Internal Error'
+			} );
 			res.end();
 			return;
 		}
-		res.json(data);
-	});
-});
+		res.json( data );
+	} );
+} );
