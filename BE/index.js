66eadc0f4c47ae71caefca6c10e3f84075d1e492
Andynopol <andrei.nechita95@gmail.com>
2020-09-11 20:28:42 +0300

    Update index.js

--- BE/index.js
+++ BE/index.js
@@ -2,9 +2,9 @@ const express = require( 'express' );
 const Datastore = require( 'nedb' );
 const base64_manager = require( './lib/base64_manager' );
 const File = require( './lib/file' );
-const fetch = require('node-fetch');
+const fetch = require( 'node-fetch' );
 
-require('dotenv').config();
+require( 'dotenv' ).config();
 
 const app = express();
 const database = new Datastore( 'data.db' );
@@ -48,14 +48,14 @@ app.get( '/api', ( req, res ) => {
 	} );
 } );
 
-app.get('/weather/:position', async (req, res)=>{
-	const position = req.params.position.split(',');
-	const lat = position[0];
-	const lon = position[1];
+app.get( '/weather/:position', async ( req, res ) => {
+	const position = req.params.position.split( ',' );
+	const lat = position[ 0 ];
+	const lon = position[ 1 ];
 	const key = process.env.API_KEY;
 	const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${key}`;
-	const api_res = await fetch(url);
+	const api_res = await fetch( url );
 	res_json = await api_res.json();
-	console.log(res_json);
-	res.send(res_json);
-});
+	// console.log(res_json);
+	res.send( res_json );
+} );
